5bf69c9a410ad7a932e3f5b0c2baa9726cb759f4
Ernst <ernstchristia@gmail.com>
2024-10-30 09:50:41 +0800

    build LoginForm UI component

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,32 @@
+// src/components/LoginForm.tsx
+import React, { useState } from 'react';
+import { useAuthAPI } from '../hooks/useAuth';
+
+export const LoginForm = () => {
+  const { login } = useAuthAPI();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
